2d636bd063063b5e9b2a585031badcbb4af000ce
Yeider Adrian Mina Caicedo <yeideradrian.nobaqgmail.com>
2024-07-08 10:13:02 -0500

    ordenamiento

--- database/factories/Customers/CustomerFactory.php
+++ database/factories/Customers/CustomerFactory.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Database\Factories;
+namespace Database\Factories\Customers;
 
 use App\Models\Customers\Customer;
 use Illuminate\Database\Eloquent\Factories\Factory;
--- database/factories/Services/PlanFactory.php
+++ database/factories/Services/PlanFactory.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Database\Factories;
+namespace Database\Factories\Services;
 
 use App\Models\Services\Plan;
 use Illuminate\Database\Eloquent\Factories\Factory;
