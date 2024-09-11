b4ad1560cc9966a62c4ce7aca87dc529d805b4f3
Juan Jose <juanjosecaicedo6@gmail.com>
2024-09-10 19:07:55 -0500

    Mejoras en la interfaz

--- resources/js/pages/CustomerAccount/Index.vue
+++ resources/js/pages/CustomerAccount/Index.vue
@@ -11,118 +11,69 @@ defineProps({sidebar: Object, customer: Object, route_edit_customer: String})
     <h1 class="text-3xl font-semibold text-slate-950">My Account</h1>
     <h2 class="text-2xl mt-5 md:mt-10 font-light">Account information</h2>
     <hr class="my-2 border-gray-300">
-    <div class="grid grid-cols-2 mt-3 md:mt-7">
-      <div class="flex flex-col">
-        <h3 class="font-semibold">Details</h3>
-        <div
-          class="flex flex-col -mx-6 px-6 py-2 space-y-2 md:flex-row  @md/modal:flex-row md:py-0  md:space-y-0">
-          <div class="md:w-1/4  md:py-3 ">
-            <h4 class="font-normal ">
-              <span>First Name:</span>
-            </h4>
 
-          </div>
-          <div class="break-all md:w-3/4  @md/modal:w-3/4 md:py-3 lg:break-words ">
-            <p class="flex items-center">
-              {{ customer.first_name }}
-            </p>
-          </div>
+    <div class="flex-1 overflow-auto p-6">
+
+
+      <dl class="grid grid-cols-1 gap-4 sm:grid-cols-2">
+        <div>
+          <dt class="text-sm font-medium text-gray-500">First Name:</dt>
+          <dd class="text-sm text-gray-900">
+            {{ customer.first_name }}
+          </dd>
+        </div>
+        <div>
+          <dt class="text-sm font-medium text-gray-500">Last Name:</dt>
+          <dd class="text-sm text-gray-900">
+            {{ customer.last_name }}
+          </dd>
+        </div>
+        <div>
+          <dt class="text-sm font-medium text-gray-500">Date of Birth:</dt>
+          <dd class="text-sm text-gray-900">
+            {{ customer.date_of_birth ? customer.date_of_birth_formatted : '—' }}
+          </dd>
+        </div>
+        <div>
+          <dt class="text-sm font-medium text-gray-500">Phone Number</dt>
+          <dd class="text-sm text-gray-900">
+            {{ customer.phone_number ? customer.phone_number : '—' }}
+          </dd>
+        </div>
+        <div>
+          <dt class="text-sm font-medium text-gray-500">Email Address</dt>
+          <dd class="text-sm text-gray-900">
+            {{ customer.email_address }}
+          </dd>
         </div>
 
-        <div
-          class="flex flex-col -mx-6 px-6 py-2 space-y-2 md:flex-row  @md/modal:flex-row md:py-0  md:space-y-0">
-          <div class="md:w-1/4  md:py-3 ">
-            <h4 class="font-normal ">
-              <span>Last Name:</span>
-            </h4>
-
-          </div>
-          <div class="break-all md:w-3/4  @md/modal:w-3/4 md:py-3 lg:break-words ">
-            <p class="flex items-center">
-              {{ customer.last_name }}
-            </p>
-          </div>
+        <div>
+          <dt class="text-sm font-medium text-gray-500">Document Type</dt>
+          <dd class="text-sm text-gray-900">
+            {{ customer.document_type }}
+          </dd>
         </div>
 
-        <div
-          class="flex flex-col -mx-6 px-6 py-2 space-y-2 md:flex-row  @md/modal:flex-row md:py-0  md:space-y-0">
-          <div class="md:w-1/4  md:py-3 ">
-            <h4 class="font-normal ">
-              <span>Date of Birth:</span>
-            </h4>
-
-          </div>
-          <div class="break-all md:w-3/4  @md/modal:w-3/4 md:py-3 lg:break-words ">
-            <p class="flex items-center">
-              {{ customer.date_of_birth ? customer.date_of_birth_formatted : '—' }}
-            </p>
-          </div>
+        <div>
+          <dt class="text-sm font-medium text-gray-500">Identity Document</dt>
+          <dd class="text-sm text-gray-900">
+            {{ customer.identity_document }}
+          </dd>
         </div>
 
-        <div
-          class="flex flex-col -mx-6 px-6 py-2 space-y-2 md:flex-row  @md/modal:flex-row md:py-0  md:space-y-0">
-          <div class="md:w-1/4  md:py-3 ">
-            <h4 class="font-normal ">
-              <span>Phone Number</span>
-            </h4>
+        <div>
+          <dt class="text-sm font-medium text-gray-500"></dt>
+          <dd class="text-sm text-gray-900">
 
-          </div>
-          <div class="break-all md:w-3/4  @md/modal:w-3/4 md:py-3 lg:break-words ">
-            <p class="flex items-center">
-              {{ customer.phone_number ? customer.phone_number : '—' }}
-            </p>
-          </div>
+          </dd>
         </div>
 
-        <div
-          class="flex flex-col -mx-6 px-6 py-2 space-y-2 md:flex-row  @md/modal:flex-row md:py-0  md:space-y-0">
-          <div class="md:w-1/4  md:py-3 ">
-            <h4 class="font-normal ">
-              <span>Email Address</span>
-            </h4>
-
-          </div>
-          <div class="break-all md:w-3/4  @md/modal:w-3/4 md:py-3 lg:break-words ">
-            <p class="flex items-center">
-              {{ customer.email_address }}
-            </p>
-          </div>
-        </div>
-
-        <div
-          class="flex flex-col -mx-6 px-6 py-2 space-y-2 md:flex-row  @md/modal:flex-row md:py-0  md:space-y-0">
-          <div class="md:w-1/4  md:py-3 ">
-            <h4 class="font-normal ">
-              <span>Document Type</span>
-            </h4>
-
-          </div>
-          <div class="break-all md:w-3/4  @md/modal:w-3/4 md:py-3 lg:break-words ">
-            <p class="flex items-center">
-              {{ customer.document_type }}
-            </p>
-          </div>
-        </div>
-
-        <div
-          class="flex flex-col -mx-6 px-6 py-2 space-y-2 md:flex-row  @md/modal:flex-row md:py-0  md:space-y-0">
-          <div class="md:w-1/4  md:py-3 ">
-            <h4 class="font-normal ">
-              <span>Identity Document</span>
-            </h4>
-          </div>
-          <div class="break-all md:w-3/4  @md/modal:w-3/4 md:py-3 lg:break-words ">
-            <p class="flex items-center">
-              {{ customer.identity_document }}
-            </p>
-          </div>
-        </div>
-        <a  :href="route_edit_customer" class="underline">edit</a>
+      </dl>
+      <div class="mt-6">
+        <a :href="route_edit_customer"
+           class="py-2.5 px-5 me-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Edit</a>
       </div>
     </div>
   </Layout>
 </template>
 
-<style scoped>
-
-</style>
