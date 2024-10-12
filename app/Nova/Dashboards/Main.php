b3305e8f6abbf99da4ffdc28b10972364a99710b
Yeider Adrian Mina Caicedo <yeideradrian.nobaqgmail.com>
2024-10-12 08:47:24 -0500

    Dashboard

--- app/Nova/Dashboards/Main.php
+++ app/Nova/Dashboards/Main.php
@@ -2,6 +2,11 @@
 
 namespace App\Nova\Dashboards;
 
+use App\Nova\Metrics\Invoice\InvoicesStatus;
+use App\Nova\Metrics\Invoice\MonthlyRevenue;
+use App\Nova\Metrics\Invoice\OutstandingBalance;
+use App\Nova\Metrics\Invoice\TotalRevenue;
+use App\Nova\Metrics\NewCustomers;
 use Laravel\Nova\Cards\Help;
 use Laravel\Nova\Dashboards\Main as Dashboard;
 
@@ -15,7 +20,11 @@ class Main extends Dashboard
     public function cards()
     {
         return [
-            new Help,
+            (new NewCustomers())->width('1/2'),
+            (new InvoicesStatus())->width('1/2'),
+            (new TotalRevenue())->width('1/2'),
+            (new OutstandingBalance())->width('1/2'),
+            (new MonthlyRevenue())->width('1/2'),
         ];
     }
 }
