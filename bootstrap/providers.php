e935663c13d2b0c7f79b8bd052010cab58eb959c
Yeider Adrian Mina <yeideradrian.noba@gmail.com>
2024-11-29 23:35:52 -0500

    Load

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,7 +2,7 @@
 
 return [
     App\Providers\AppServiceProvider::class,
-    App\Providers\NovaServiceProvider::class,
-    App\Providers\CustomPageBuilderServiceProvider::class,
-    \Ispgo\Wiivo\WiivoServiceProvider::class
+    //App\Providers\NovaServiceProvider::class,
+    //App\Providers\CustomPageBuilderServiceProvider::class,
+    //\Ispgo\Wiivo\WiivoServiceProvider::class
 ];
