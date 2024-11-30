d659ed243c4a3c57995cbf9efdd609f1b6234321
Yeider Adrian Mina <yeideradrian.noba@gmail.com>
2024-11-30 10:33:50 -0500

    Configuracion de nginx

--- start.sh
+++ start.sh
@@ -1,2 +1,10 @@
-#!/bin/bash
-mkdir -p /var/log/nginx
+#!/bin/sh
+
+# Ejecutar migraciones
+php artisan migrate --force
+
+# Iniciar PHP-FPM
+php-fpm -D
+
+# Iniciar Nginx en primer plano
+nginx -g "daemon off;"
