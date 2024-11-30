48b5ba99205f5a92ddff3a27b21f65be8f39f144
Yeider Adrian Mina <yeideradrian.noba@gmail.com>
2024-11-30 12:27:37 -0500

    Css

--- vite.config.ts
+++ vite.config.ts
@@ -4,17 +4,19 @@ import react from '@vitejs/plugin-react';
 import path from "path"
 
 export default defineConfig({
-    plugins: [
-
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.tsx'],
-            refresh: true,
-        }),
-        react(),
-    ],
-    resolve: {
-        alias: {
-            '@': path.resolve(__dirname, './resources/js')
-        }
+  plugins: [
+    laravel({
+      input: ['resources/css/app.css', 'resources/js/app.tsx'],
+      refresh: true,
+    }),
+    react(),
+  ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './resources/js')
     }
+  },
+  server: {
+    https: true, // Fuerza el uso de HTTPS
+  },
 });
