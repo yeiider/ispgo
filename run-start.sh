77c1f4f3598427cb7acacb0965714d43c464c3de
Yeider Adrian Mina <yeideradrian.noba@gmail.com>
2025-04-10 20:01:49 -0500

    Token anual

--- run-start.sh
+++ run-start.sh
@@ -1,21 +1,5 @@
 #!/bin/bash
 
-PRIVATE_KEY_PATH="storage/oauth-private.key"
-PUBLIC_KEY_PATH="storage/oauth-public.key"
-
-if [[ ! -f "$PRIVATE_KEY_PATH" || ! -f "$PUBLIC_KEY_PATH" ]]; then
-  echo "Las claves RSA no existen. Generando nuevas claves..."
-  php artisan passport:keys --force
-  if [[ $? -eq 0 ]]; then
-    echo "Claves RSA generadas exitosamente."
-  else
-    echo "Error al generar las claves RSA."
-    exit 1
-  fi
-else
-  echo "Las claves RSA ya existen. No es necesario generarlas."
-fi
-
 # Ejecutar las demás tareas
 echo "Ejecutando el build de la aplicación..."
 npm run build
