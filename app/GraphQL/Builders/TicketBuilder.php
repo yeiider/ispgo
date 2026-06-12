f4c3830c63f7c0c3c152ca7642b0bcfac8f59e5b
Stiven Gonzalez <juanstiven.developer@gmail.com>
2026-06-12 11:32:46 -0500

    graphql: actualizacion ticket, filtros en clientes, servicios y facturas

--- app/GraphQL/Builders/TicketBuilder.php
+++ app/GraphQL/Builders/TicketBuilder.php
@@ -11,6 +11,8 @@ class TicketBuilder
     {
         $query = Ticket::query();
 
+        $recentOnly = $args['recent_only'] ?? true;
+
         if (isset($args['title']) && !empty($args['title'])) {
             // Frontend passes '%searchQuery%' when searching by title
             $search = trim($args['title'], '%'); 
@@ -25,9 +27,36 @@ class TicketBuilder
                          ->orWhereRaw("CONCAT(first_name, ' ', last_name) LIKE ?", ["%{$search}%"]);
                   });
             });
-        } else {
-            // If no search is provided, only show recent tickets (30 days)
+        } elseif ($recentOnly) {
+            // If recent_only is true (default), only show recent tickets (30 days)
             $query->recent();
+        }
+
+        if (isset($args['router_id']) && !empty($args['router_id'])) {
+            $routerId = $args['router_id'];
+            $query->where(function ($q) use ($routerId) {
+                $q->whereHas('customer', function ($q2) use ($routerId) {
+                    $q2->where('router_id', $routerId);
+                })->orWhereHas('service', function ($q2) use ($routerId) {
+                    $q2->where('router_id', $routerId);
+                });
+            });
+        }
+
+        if (isset($args['status']) && !empty($args['status']) && $args['status'] !== 'all') {
+            $query->where('status', $args['status']);
+        }
+
+        if (isset($args['priority']) && !empty($args['priority']) && $args['priority'] !== 'all') {
+            $query->where('priority', $args['priority']);
+        }
+
+        if (isset($args['customer_id']) && !empty($args['customer_id'])) {
+            $query->where('customer_id', $args['customer_id']);
+        }
+
+        if (isset($args['service_id']) && !empty($args['service_id'])) {
+            $query->where('service_id', $args['service_id']);
         }
 
         return $query;
